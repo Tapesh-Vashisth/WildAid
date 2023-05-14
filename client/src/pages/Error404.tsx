13e5a574dd41c197783d17607736a36dde66825e
Prasad <prasadjs178@gmail.com>
2023-05-14 14:19:50 +0530

    added 404

--- client/src/pages/Error404.tsx
+++ client/src/pages/Error404.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styles from '../styles/404.module.css'
 
 function Error404() {
   return (
-    <div>Error404</div>
+    <>
+      {/* <b:if cond="data:blog.pageType != &quot;error_page&quot;"></b:if> */}
+    {/* <b:if cond="data:blog.pageType == &quot;error_page&quot;"> */}
+    <div>
+      <img src='/images/image.png'></img>
+      <div id={styles.buttonerror}>
+        <a href="#">Homepage</a>
+      </div>
+    </div>
+    {/* </b:if> */}
+    </>
   )
 }
 
