13021eda9b52910e7fe8b0cbf9d5addee93b2338
tapesh <tapeshvashisth789@gmail.com>
2023-05-15 12:38:06 +0530

    server config

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ const app: Express = express();
 // middleware
 app.use(express.json({limit: '50mb'}));
 app.use(cors({
-    origin:["http://localhost:3000", "https://stock-hub.netlify.app"],
+    origin:["http://localhost:3000", "https://wildaid.netlify.app"],
     methods:['POST','GET','HEAD','PUT','DELETE'],
     credentials: true
 }))
